ab26f7315e657c769d70b7c9aa31925df39c8646
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-10-04 16:18:46 -0600

    finalize 1.3.0 release

--- gemspec.rb
+++ gemspec.rb
@@ -23,7 +23,7 @@ TyrantManager::GEM_SPEC = Gem::Specification.new do |spec|
   spec.add_dependency( "loquacious", "~> 1.3.0")
   spec.add_dependency( "rufus-tokyo", "~> 1.0.0")
   spec.add_dependency( "logging", "~> 1.2.2" )
-  spec.add_dependency( "main", "~> 2.9.0" )
+  spec.add_dependency( "main", "~> 2.9.3" )
 
   # development dependencies
   spec.add_development_dependency("configuration", "~> 1.1.0")
