787b2a1d6490a6254c9572ad033afb64dc7fd2ba
Jeremy Hinegardner <jeremy@hinegardner.org>
2010-01-21 12:17:57 -0700

    add additional tyrant instances for testing patterns

--- spec/tyrant_instance_spec.rb
+++ spec/tyrant_instance_spec.rb
@@ -5,17 +5,30 @@ require 'tyrant_manager/tyrant_instance'
 
 describe TyrantManager::TyrantInstance  do
   before( :each ) do
-    @tdir = File.join( temp_dir, 'tyrant' )
+    @tyrant_manager_dir = File.join( temp_dir, 'tyrant' )
+
+    @instances = {
+        'standalone'       => nil,
+        'slave_1'          => nil,
+        'master_master_1'  => nil,
+        'master_master_2'  => nil 
+      }
+ 
     TyrantManager::Log.silent {
-      @mgr = TyrantManager.setup( @tdir )
-      @idir   = @mgr.instances_path( "test" )
-      @tyrant = TyrantManager::TyrantInstance.setup( @idir )
-      @tyrant.manager = @mgr
+
+      @mgr = TyrantManager.setup( @tyrant_manager_dir)
+
+     @instances.keys.each do |name|
+        idir = @mgr.instances_path( name )
+        tyrant = TyrantManager::TyrantInstance.setup( idir )
+        tyrant.manager = @mgr
+        @instances[name] = tyrant
+      end
     }
   end
 
   after( :each ) do
-    FileUtils.rm_rf @tdir
+    FileUtils.rm_rf @tyrant_manager_dir
   end
 
   it "raises an exception if given an invalid directory on initialization" do
@@ -23,53 +36,59 @@ describe TyrantManager::TyrantInstance  do
   end
 
   it "#config_file" do
-    @tyrant.config_file.should == File.join( @tdir, "instances", "test", "config.rb" )
-    File.exist?( @tyrant.config_file ).should == true
+    @instances['standalone'].config_file.should == File.join( @tyrant_manager_dir, "instances", "standalone", "config.rb" )
+    File.exist?( @instances['standalone'].config_file ).should == true
   end
 
   it "#configuration" do
-    @tyrant.configuration.should_not == nil
+    @instances['standalone'].configuration.should_not == nil
   end
 
   it "#pid_file" do
-    @tyrant.pid_file.should == File.join( @tdir, "instances", "test", "test.pid" )
+    @instances['standalone'].pid_file.should == File.join( @tyrant_manager_dir, "instances", "standalone", "standalone.pid" )
   end
 
   it "#log_file" do
-    @tyrant.log_file.should == File.join( @tdir, "instances", "test", "log", "test.log" )
+    @instances['standalone'].log_file.should == File.join( @tyrant_manager_dir, "instances", "standalone", "log", "standalone.log" )
   end
 
   it "#replication_timestamp_file" do
-    @tyrant.replication_timestamp_file.should == File.join( @tdir, "instances", "test", "test.rts" )
+    @instances['standalone'].replication_timestamp_file.should == File.join( @tyrant_manager_dir, "instances", "standalone", "standalone.rts" )
   end
 
   it "#lua_extension_file" do
-    @tyrant.lua_extension_file.should == File.join( @tdir, "instances", "test", "lua", "test.lua" )
+    @instances['standalone'].lua_extension_file.should == File.join( @tyrant_manager_dir, "instances", "standalone", "lua", "standalone.lua" )
   end
 
   it "#data_dir" do
-    @tyrant.data_dir.should == File.join( @tdir, "instances", "test", "data" )
+    @instances['standalone'].data_dir.should == File.join( @tyrant_manager_dir, "instances", "standalone", "data" )
   end
 
   describe "against running instances" do
     before( :each ) do
-      @tyrant.start
+      @instances['standalone'].start
       start = Time.now
       loop do
-        break if @tyrant.running?
+        break if @instances['standalone'].running?
         sleep 0.1
         break if (Time.now - start) > 2
       end
     end
 
     after( :each ) do
-      @tyrant.stop
+      @instances['standalone'].stop
     end
 
-    it "#is_slave?" do
-      @tyrant.running?.should == true
-      @tyrant.should_not be_is_slave
-      @tyrant.stop
+    it "#is_slave? when it is not a slave" do
+      @instances['standalone'].running?.should == true
+      @instances['standalone'].should_not be_is_slave
+      @instances['standalone'].stop
+    end
+
+    it "#is_slave? when it IS a slave" do
+      @instances['standalone'].running?.should == true
+      @instances['standalone'].should be_is_slave
+      @instances['standalone'].stop
     end
 
   end
@@ -84,23 +103,23 @@ describe TyrantManager::TyrantInstance  do
       'table'       => "tct"
     }.each_pair do |db_type, db_ext|
       it "db_type of #{db_type} is db file with ext #{db_ext}" do
-        f = @tyrant.db_file( db_type )
+        f = @instances['standalone'].db_file( db_type )
         if f.size == 1 then
           f.should == db_ext
         else
-          f.should == File.join( @tdir, "instances", "test", "data", "test.#{db_ext}" )
+          f.should == File.join( @tyrant_manager_dir, "instances", "standalone", "data", "standalone.#{db_ext}" )
         end
       end
     end
   end
 
   it "#ulog_dir" do
-    @tyrant.ulog_dir.should == File.join( @tdir, "instances", "test", "ulog" )
+    @instances['standalone'].ulog_dir.should == File.join( @tyrant_manager_dir, "instances", "standalone", "ulog" )
   end
 
 
   it "#start_command" do
-    @tyrant.start_command.should =~ /^ttserver/
+    @instances['standalone'].start_command.should =~ /^ttserver/
   end
 
 end
