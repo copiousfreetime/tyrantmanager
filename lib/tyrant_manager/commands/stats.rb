83adf391c4d481b5c40cbb286bc9a188b4857590
Jeremy Hinegardner <jeremy@hinegardner.org>
2010-01-21 20:07:29 -0700

    adjust number of trailing .. according to maximum width of stat names

--- lib/tyrant_manager/commands/stats.rb
+++ lib/tyrant_manager/commands/stats.rb
@@ -9,8 +9,9 @@ class TyrantManager
         manager.each_instance( options['instances'] ) do |instance|
           puts "Instance #{instance.name} at #{instance.home_dir}"
           stats = instance.stat
+          max_width = stats.keys.collect { |k| k.length }.sort.last + 2
           stats.keys.sort.each do |k|
-            lhs = k.ljust(10, ".")
+            lhs = k.ljust(max_width, ".")
             puts "    #{lhs} #{stats[k]}"
           end
           puts
