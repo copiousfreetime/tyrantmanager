172e8f8a4f6f93ddd98a44924754c5b615840593
Jeremy Hinegardner <jeremy@hinegardner.org>
2010-05-19 14:02:25 -0600

    bump logging requiremnts

--- gemspec.rb
+++ gemspec.rb
@@ -22,7 +22,7 @@ TyrantManager::GEM_SPEC = Gem::Specification.new do |spec|
   # add dependencies here
   spec.add_dependency( "loquacious", "~> 1.3.0")
   spec.add_dependency( "rufus-tokyo", "~> 1.0.0")
-  spec.add_dependency( "logging", "~> 1.3.0" )
+  spec.add_dependency( "logging", "~> 1.4.0" )
   spec.add_dependency( "main", "~> 4.2.0" )
 
   # development dependencies
